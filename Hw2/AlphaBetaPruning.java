262c53c88a00965ad3d62b20435f7938fa6c898a
Johnsun <lsun89@wisc.edu>
2019-10-05 21:08:53 -0500

    Modified files

--- Hw2/AlphaBetaPruning.java
+++ Hw2/AlphaBetaPruning.java
@@ -1,133 +1,188 @@
+import java.util.ArrayList;
+
+
 public class AlphaBetaPruning {
-    
-    private int move;
-    private double value;
-    private int nodesvisited;
-    private int nodesevaluated;
-    private int depthreached;
-    private double bf = 0.0;
- 
-    
-    private int realvisited;
-    private int realevaluated;
-    private int realdepth;
-    private double realbf = 0.0;
 
-    public AlphaBetaPruning() {
-    }
+  private int move;
+  private double value;
+  private int nodesvisited;
+  private int nodesevaluated;
+  private int depthreached;
+  private ArrayList<Double> list = new ArrayList<>();
 
-    /**
-     * This function will print out the information to the terminal,
-     * as specified in the homework description.
-     */
-    public void printStats() {
-     
-      System.out.println("Move: " + move);
-      System.out.println("Value: " + value);
-      System.out.println("Number of Nodes Visited: " + realvisited);
-      System.out.println("Number of Nodes Evaluated: " + realevaluated);
-      System.out.println("Max Depth Reached: " + realdepth);
-      System.out.println("Avg Effective Branching Factor: " + realbf);
-      System.out.println("");
-      
-    }
+  private ArrayList<Double> branchingfactor = new ArrayList<>();
 
-    /**
-     * This function will start the alpha-beta search
-     * @param state This is the current game state
-     * @param depth This is the specified search depth
-     */
-    public void run(GameState state, int depth) {
-      
-     
-     
-     
-        if(state.countStonesTaken() % 2 != 0) {
-          value = alphabeta(state, 0, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, false, depth);
-          
-          realvisited = nodesvisited;
-          realevaluated = nodesevaluated;
-          realdepth = depthreached;
-          
-          for(int i = 0; i < state.getSuccessors().size(); i++) {
 
-            double curr = alphabeta(state.getSuccessors().get(i), 1, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, true, depth);
+  private int realvisited;
+  private int realevaluated;
+  private int realdepth;
+  private double actualbf = 0.0;
 
-            if(curr == value) {
-              move = state.getSuccessors().get(i).getLastMove();
-              break;
-            }
-          }
+  public AlphaBetaPruning() {}
 
-        } else {
-          value = alphabeta(state, 0, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, true, depth);
-          
-          realvisited = nodesvisited;
-          realevaluated = nodesevaluated;
-          realdepth = depthreached;
-          
-          for(int i = 0; i < state.getSuccessors().size(); i++) {
+  /**
+   * This function will print out the information to the terminal, as specified in the homework
+   * description.
+   */
+  public void printStats() {
 
-            double curr = alphabeta(state.getSuccessors().get(i), 1, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, false, depth);
+    System.out.println("Move: " + move);
+    System.out.println("Value: " + value);
+    System.out.println("Number of Nodes Visited: " + realvisited);
+    System.out.println("Number of Nodes Evaluated: " + realevaluated);
+    System.out.println("Max Depth Reached: " + realdepth);
+    System.out.println("Avg Effective Branching Factor: " + String.format("%.1f", actualbf));
+    System.out.println("");
 
-            if(curr == value) {
-              move = state.getSuccessors().get(i).getLastMove();
-              break;
-            }
-          }
-          
+  }
+
+  /**
+   * This function will start the alpha-beta search
+   * 
+   * @param state This is the current game state
+   * @param depth This is the specified search depth
+   */
+  public void run(GameState state, int depth) {
+
+
+
+    if (state.countStonesTaken() % 2 != 0) {
+      value = alphabeta(state, 0, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, false, depth);
+
+      realvisited = nodesvisited;
+      realevaluated = nodesevaluated;
+      realdepth = depthreached;
+      int count = 0;
+      double sum = 0.0;
+      for (int i = 0; i < branchingfactor.size(); i++) {
+        sum += branchingfactor.get(i);
+        count++;
+      }
+
+      actualbf = sum / count;
+
+
+      for (int i = 0; i < state.getSuccessors().size(); i++) {
+
+        double curr = alphabeta(state.getSuccessors().get(i), 1, Double.NEGATIVE_INFINITY,
+            Double.POSITIVE_INFINITY, true, depth);
+
+        if (curr == value) {
+          move = state.getSuccessors().get(i).getLastMove();
+          break;
         }
-        
-        System.out.println("");
-    }
+      }
 
-    /**
-     * This method is used to implement alpha-beta pruning for both 2 players
-     * @param state This is the current game state
-     * @param depth Current depth of search
-     * @param alpha Current Alpha value
-     * @param beta Current Beta value
-     * @param maxPlayer True if player is Max Player; Otherwise, false
-     * @return double This is the number indicating score of the best next move
-     */
-    private double alphabeta(GameState state, int depth, double alpha, double beta, boolean maxPlayer, int maxdepth) {
-        
-        nodesvisited++;
-        depthreached = Math.max(depthreached, depth);
+      // int index = list.indexOf(value);
+      // move = state.getSuccessors().get(index).getLastMove();
 
-        if(state.getSuccessors().size() == 0 || depth == maxdepth) {
-          nodesevaluated++;
-          return state.evaluate();
-        }
-        if(maxPlayer) {
-          double v = Double.NEGATIVE_INFINITY;
-          for(int i = 0; i < state.getSuccessors().size(); i++) {
-            
-            v = Math.max(v, alphabeta(state.getSuccessors().get(i), depth + 1, alpha, beta, !maxPlayer, maxdepth));
-        
-            if(v >= beta) {
 
-              return v;
-            }
-            alpha = Math.max(alpha, v);
-          }
-          return v;
-        } else {
-          double v = Double.POSITIVE_INFINITY;
-          for(int i = 0; i < state.getSuccessors().size(); i++) {
-            
-            v = Math.min(v, alphabeta(state.getSuccessors().get(i), depth + 1, alpha, beta, !maxPlayer, maxdepth)); // min alphabeta 
+    } else {
+      value = alphabeta(state, 0, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, true, depth);
 
-            if(v <= alpha) 
-              {
-              if(nodesvisited == 1) move = state.getSuccessors().get(i).getLastMove();
-              return v;              
-              }
-            beta = Math.min(beta, v);
-          }
-          return v;
-        }
+      realvisited = nodesvisited;
+      realevaluated = nodesevaluated;
+      realdepth = depthreached;
+
+      int count = 0;
+      double sum = 0.0;
+      for (int i = 0; i < branchingfactor.size(); i++) {
+        sum += branchingfactor.get(i);
+        count++;
+      }
+
+      actualbf = sum / count;
+
       
+      for (int i = 0; i < state.getSuccessors().size(); i++) {
+
+        double curr = alphabeta(state.getSuccessors().get(i), 1, Double.NEGATIVE_INFINITY,
+            Double.POSITIVE_INFINITY, false, depth);
+
+        if (curr == value) {
+          move = state.getSuccessors().get(i).getLastMove();
+          break;
+        }
+      }
+
+      // int index = list.indexOf(value);
+      // move = state.getSuccessors().get(index).getLastMove();
+
+
+
     }
-    
+
+  }
+
+  /**
+   * This method is used to implement alpha-beta pruning for both 2 players
+   * 
+   * @param state This is the current game state
+   * @param depth Current depth of search
+   * @param alpha Current Alpha value
+   * @param beta Current Beta value
+   * @param maxPlayer True if player is Max Player; Otherwise, false
+   * @return double This is the number indicating score of the best next move
+   */
+  private double alphabeta(GameState state, int depth, double alpha, double beta, boolean maxPlayer,
+      int maxdepth) {
+
+    nodesvisited++;
+    depthreached = Math.max(depthreached, depth);
+
+    if (state.getSuccessors().size() == 0 || depth == maxdepth) {
+      nodesevaluated++;
+      return state.evaluate();
+    }
+
+    double bf = 0.0;
+
+    if (maxPlayer) {
+      double v = Double.NEGATIVE_INFINITY;
+      for (int i = 0; i < state.getSuccessors().size(); i++) {
+        bf++;
+        v = Math.max(v,
+            alphabeta(state.getSuccessors().get(i), depth + 1, alpha, beta, !maxPlayer, maxdepth));
+
+        // double curr = alphabeta(state.getSuccessors().get(i), depth + 1, alpha, beta, !maxPlayer,
+        // maxdepth);
+        // v = Math.min(curr, v);
+        //
+        // if(depth == 1) list.add(curr);
+
+        if (v >= beta) {
+
+          break;
+        }
+        alpha = Math.max(alpha, v);
+      }
+      // if(depth == 1)
+      // {list.add(v);}
+      branchingfactor.add(bf);
+      return v;
+    } else {
+      double v = Double.POSITIVE_INFINITY;
+      for (int i = 0; i < state.getSuccessors().size(); i++) {
+        bf++;
+        v = Math.min(v,
+            alphabeta(state.getSuccessors().get(i), depth + 1, alpha, beta, !maxPlayer, maxdepth)); // min
+                                                                                                    // alphabeta
+        // double curr = alphabeta(state.getSuccessors().get(i), depth + 1, alpha, beta, !maxPlayer,
+        // maxdepth);
+        // v = Math.min(curr, v);
+        // if(depth == 1) list.add(curr);
+
+        if (v <= alpha) {
+          break;
+        }
+        beta = Math.min(beta, v);
+      }
+      // if(depth == 1)
+      // {list.add(v);}
+      branchingfactor.add(bf);
+      return v;
+    }
+
+  }
+
 }
--- Hw2/GameState.java
+++ Hw2/GameState.java
@@ -13,7 +13,7 @@ public class GameState {
    */
   public GameState(int size) {
 
-    this.size = size; 
+    this.size = size;
 
     // For convenience, we use 1-based index, and set 0 to be unavailable
     this.stones = new boolean[this.size + 1];
@@ -48,7 +48,7 @@ public class GameState {
     List<Integer> list = new ArrayList<>();
     if (lastMove == -1) {
       for (int i = 1; i <= this.size; i++) {
-        if((i < size/2.0) && (i % 2 != 0))
+        if ((i < size / 2.0) && (i % 2 != 0))
           list.add(i);
       }
     } else {
@@ -66,7 +66,8 @@ public class GameState {
   // x is a multiple of y
   private static boolean isMultiple(int x, int y, int size) {
 
-    if(x % y == 0) return true;
+    if (x % y == 0)
+      return true;
     return false;
   }
 
@@ -114,15 +115,17 @@ public class GameState {
     } else {
       int primefactor = Helper.getLargestPrimeFactor(lastMove);
       int count = countMultiples(primefactor);
-      if(stones[primefactor]) count++;
+      if (stones[primefactor])
+        count++;
       if (count % 2 != 0)
         result = 0.6;
       else
         result = -0.6;
     }
 
-     if (countStonesTaken() % 2 != 0) result = result * (-1);
-    
+    if (countStonesTaken() % 2 != 0)
+      result = result * (-1);
+
     return result;
   }
 
@@ -138,7 +141,7 @@ public class GameState {
   public int countMultiples(int x) {
 
     int count = 0;
-    for (GameState s: getSuccessors()) {
+    for (GameState s : getSuccessors()) {
       if (isMultiple(s.getLastMove(), x, this.size))
         count++;
     }
